89b70fe15cfe6e0c34f62d8b09095c7ac501cb98
RhodyArthur <rhodaarthur48@gmail.com>
2024-08-12 20:28:32 +0100

    completes contact info ui

--- src/app/components/main-layout/main-layout.component.ts
+++ src/app/components/main-layout/main-layout.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { PersonalInfoComponent } from "../personal-info/personal-info.component";
+import { ContactInfoComponent } from '../contact-info/contact-info.component';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [PersonalInfoComponent],
+  imports: [PersonalInfoComponent,
+            ContactInfoComponent
+  ],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.css'
 })
