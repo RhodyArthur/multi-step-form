a321bd0ab35f57fab75a43eee31048b16054861b
RhodyArthur <rhodaarthur48@gmail.com>
2024-08-12 12:13:52 +0100

    adds pagination link

--- src/app/components/main-layout/main-layout.component.ts
+++ src/app/components/main-layout/main-layout.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { PersonalInfoComponent } from "../personal-info/personal-info.component";
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [],
+  imports: [PersonalInfoComponent],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.css'
 })
