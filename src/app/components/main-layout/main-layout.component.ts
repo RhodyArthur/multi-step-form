1d1d11458de37175a0726f3170ea909d6882a9e1
RhodyArthur <rhodaarthur48@gmail.com>
2024-08-13 22:59:35 +0100

    Completes educational background ui

--- src/app/components/main-layout/main-layout.component.ts
+++ src/app/components/main-layout/main-layout.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { PersonalInfoComponent } from "../personal-info/personal-info.component";
 import { ContactInfoComponent } from '../contact-info/contact-info.component';
 import { GuardianInfoComponent } from "../guardian-info/guardian-info.component";
+import { EducationBackgroundComponent } from "../education-background/education-background.component";
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
   imports: [PersonalInfoComponent,
-    ContactInfoComponent, GuardianInfoComponent],
+    ContactInfoComponent, GuardianInfoComponent, EducationBackgroundComponent],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.css'
 })
