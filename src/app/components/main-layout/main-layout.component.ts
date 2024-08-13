99923067a03856ee059b7ed16a06461994ea9f5f
RhodyArthur <rhodaarthur48@gmail.com>
2024-08-14 00:10:01 +0100

    Completes exams result layout

--- src/app/components/main-layout/main-layout.component.ts
+++ src/app/components/main-layout/main-layout.component.ts
@@ -3,12 +3,13 @@ import { PersonalInfoComponent } from "../personal-info/personal-info.component"
 import { ContactInfoComponent } from '../contact-info/contact-info.component';
 import { GuardianInfoComponent } from "../guardian-info/guardian-info.component";
 import { EducationBackgroundComponent } from "../education-background/education-background.component";
+import { ExamResultComponent } from "../exam-result/exam-result.component";
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
   imports: [PersonalInfoComponent,
-    ContactInfoComponent, GuardianInfoComponent, EducationBackgroundComponent],
+    ContactInfoComponent, GuardianInfoComponent, EducationBackgroundComponent, ExamResultComponent],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.css'
 })
